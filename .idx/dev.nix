4470187d18acde9f9593b15c4f52dc5613fb2f8a
gokuleduid <gokuleduid@gmail.com>
2024-09-09 09:32:20 +0000

    added file

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,16 +1,9 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
   channel = "stable-23.11"; # or "unstable"
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
-    # pkgs.nodePackages.nodemon
     pkgs.dotnet-sdk_8
     pkgs.dotnet-runtime_8
     pkgs.dotnet-aspnetcore_8
@@ -18,11 +11,17 @@
   ];
 
   # Sets environment variables in the workspace
-  env = {};
+  env = {
+    # Add any environment variables you need here
+  };
+
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
+      "ms-dotnettools.vscode-dotnet-runtime"
+      "muhammad-sammy.csharp"
+      "eamodio.gitlens"
+      "k--kato.docomment"
     ];
 
     # Enable previews
@@ -46,14 +45,14 @@
     workspace = {
       # Runs when a workspace is first created
       onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
+        # Example: restore .NET dependencies
+        dotnet-restore = "dotnet restore /home/user/BackendLXP/LXP.API/LXP.Api.csproj";
       };
       # Runs when the workspace is (re)started
       onStart = {
         # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
+        watch-backend = "dotnet watch --project /home/user/BackendLXP/LXP.API/LXP.Api.csproj run";
       };
     };
   };
-}
+  }
